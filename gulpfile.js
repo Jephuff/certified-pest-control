6d561920d99b31deeff1c4748b82911b4e442e4d
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2017-11-27 21:33:26 -0500

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,16 @@ const gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 const glob = require( 'glob' );
 const path = require( 'path' );
+var http = require('http');
+var ecstatic = require('ecstatic');
 
 const templateData = {};
+
 const options = {}
 
-
 glob.sync( './data/**/*.json' ).forEach(function( file ) {
-    console.log(file);
     templateData[file.replace(/\.json$/, '').replace(/^.*data\//, '')] = require( path.resolve( file ) );
 });
-console.log(templateData.home);
-
 
 gulp.task('copy-files', function() {
     return gulp.src(['src/**/*', '!src/**/*.html'])
@@ -27,3 +26,13 @@ gulp.task('copy-html', function() {
 });
 
 gulp.task('build', ['copy-files', 'copy-html']);
+
+gulp.task('default', function(){
+  http.createServer(
+    ecstatic({ root: __dirname + '/dist' })
+  ).listen(8080);
+
+  gulp.watch('src/**/*.*', function(){
+    gulp.run('build');
+  });
+});
