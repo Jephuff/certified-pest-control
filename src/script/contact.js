ed93cda3c7c45a64656c5f449f9195feea5962d5
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2017-12-03 06:19:04 -0500

    update name

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,73 +1,100 @@
 var formTypes = {
-    "Request a Free Quote": "request-form",
-    "General Inquiries": "general-form"
+  "Request a Free Quote": "request-form",
+  "General Inquiries": "general-form"
 };
 
 function onFormTypeChange(e) {
-    var classList = document.querySelector('.form form').classList;
+  var classList = document.querySelector(".form form").classList;
 
-    Object.keys(formTypes).forEach(function(key) {
-        classList.remove(formTypes[key]);
+  Object.keys(formTypes).forEach(function(key) {
+    classList.remove(formTypes[key]);
+  });
+
+  classList.add(formTypes[e.value]);
+
+  Array.prototype.slice
+    .call(document.querySelectorAll(".form .request-form .only-request [name]"))
+    .forEach(function(el) {
+      if (el.dataset.required) {
+        el.required = true;
+      }
     });
 
-    classList.add(formTypes[e.value]);
+  Array.prototype.slice
+    .call(document.querySelectorAll(".form .general-form .only-request [name]"))
+    .forEach(function(el) {
+      el.value = "";
+      if (el.required) {
+        el.required = false;
+        el.dataset.required = true;
+      }
+    });
 }
 
-
 function initMap() {
-    var latlng = new google.maps.LatLng(43.992606, -78.023462);
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: latlng,
-        zoom: 10,
-        disableDefaultUI: true,
-    });
+  var latlng = new google.maps.LatLng(43.992606, -78.023462);
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: latlng,
+    zoom: 10,
+    disableDefaultUI: true
+  });
 
-    var centered = false;
-    map.addListener('projection_changed', function () {
-        if (!centered) {
-            centered = true;
-            var center = window.innerWidth / 2;
-            var overlay = document.querySelector('.map-overlay');
-            var leftPostion = overlay.offsetWidth / 2 + 10;
-            offsetCenter(latlng, leftPostion, (overlay.offsetWidth) / 2  - 100);
-        }
-    });
-
-    function offsetCenter(latlng, offsetx, offsety) {
-
-        // latlng is the apparent centre-point
-        // offsetx is the distance you want that point to move to the right, in pixels
-        // offsety is the distance you want that point to move upwards, in pixels
-        // offset can be negative
-        // offsetx and offsety are both optional
-
-        var scale = Math.pow(2, map.getZoom());
-        var worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latlng);
-        var pixelOffset = new google.maps.Point((offsetx / scale) || 0, (offsety / scale) || 0);
-
-        var worldCoordinateNewCenter = new google.maps.Point(
-            worldCoordinateCenter.x - pixelOffset.x,
-            worldCoordinateCenter.y + pixelOffset.y
-        );
-
-        var newCenter = map.getProjection().fromPointToLatLng(worldCoordinateNewCenter);
-
-        map.setCenter(newCenter);
+  var centered = false;
+  map.addListener("projection_changed", function() {
+    if (!centered) {
+      centered = true;
+      var center = window.innerWidth / 2;
+      var overlay = document.querySelector(".map-overlay");
+      var leftPostion = overlay.offsetWidth / 2 + 10;
+      offsetCenter(latlng, leftPostion, overlay.offsetWidth / 2 - 100);
     }
+  });
 
-    var service = new google.maps.places.PlacesService(map);
+  function offsetCenter(latlng, offsetx, offsety) {
+    // latlng is the apparent centre-point
+    // offsetx is the distance you want that point to move to the right, in pixels
+    // offsety is the distance you want that point to move upwards, in pixels
+    // offset can be negative
+    // offsetx and offsety are both optional
 
-    service.getDetails({
-        placeId: 'ChIJOUaZBs7d1YkR4lHI05GKpRk'
-    }, function (place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-            google.maps.event.addListener(marker, 'click', function () {
-                window.open('https://www.google.ca/maps/place/Grafton,+ON+K0K+2G0/@43.9923162,-78.0345331,14.63z/data=!4m13!1m7!3m6!1s0x89d5ddce06994639:0x19a58a91d3c851e2!2sGrafton,+ON+K0K+2G0!3b1!8m2!3d43.992634!4d-78.023383!3m4!1s0x89d5ddce06994639:0x19a58a91d3c851e2!8m2!3d43.992634!4d-78.023383', '_BLANK')
-            });
-        }
-    });
+    var scale = Math.pow(2, map.getZoom());
+    var worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latlng);
+    var pixelOffset = new google.maps.Point(
+      offsetx / scale || 0,
+      offsety / scale || 0
+    );
+
+    var worldCoordinateNewCenter = new google.maps.Point(
+      worldCoordinateCenter.x - pixelOffset.x,
+      worldCoordinateCenter.y + pixelOffset.y
+    );
+
+    var newCenter = map
+      .getProjection()
+      .fromPointToLatLng(worldCoordinateNewCenter);
+
+    map.setCenter(newCenter);
+  }
+
+  var service = new google.maps.places.PlacesService(map);
+
+  service.getDetails(
+    {
+      placeId: "ChIJOUaZBs7d1YkR4lHI05GKpRk"
+    },
+    function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, "click", function() {
+          window.open(
+            "https://www.google.ca/maps/place/Grafton,+ON+K0K+2G0/@43.9923162,-78.0345331,14.63z/data=!4m13!1m7!3m6!1s0x89d5ddce06994639:0x19a58a91d3c851e2!2sGrafton,+ON+K0K+2G0!3b1!8m2!3d43.992634!4d-78.023383!3m4!1s0x89d5ddce06994639:0x19a58a91d3c851e2!8m2!3d43.992634!4d-78.023383",
+            "_BLANK"
+          );
+        });
+      }
+    }
+  );
 }
