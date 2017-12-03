43c109f0dcec580e80e77c722f6ae213793686fa
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2017-12-02 20:53:27 -0500

    update build tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,120 @@
-var mkdirp = require('mkdirp');
-var gulp = require('gulp');
-var handlebars = require('gulp-compile-handlebars');
-var glob = require( 'glob' );
-var path = require( 'path' );
-var http = require('http');
-var ecstatic = require('ecstatic');
-var build = require('./src/script/build'); // adds handlebar helpers
+const gulp = require("gulp");
+const handlebars = require("gulp-compile-handlebars");
+const glob = require("glob");
+const path = require("path");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var gutil = require('gulp-util');
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const gutil = require("gulp-util");
+const merge = require("merge-stream");
 
+require("./src/script/build");
+
+const isProd = process.env.NODE_ENV === "production";
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
 
 var getData = function() {
   var templateData = {};
 
-  glob.sync( './data/**/*.json' ).forEach(function( file ) {
-      templateData[file.replace(/\.json$/, '').replace(/^.*data\//, '')] = require( path.resolve( file ) );
+  glob.sync("./data/**/*.json").forEach(function(file) {
+    templateData[
+      file.replace(/\.json$/, "").replace(/^.*data\//, "")
+    ] = require(path.resolve(file));
   });
-  return templateData
-}
+  return templateData;
+};
 
 function bundle(file) {
   var b = browserify({
-    entries: './src/script/' + file,
+    entries: "./src/script/" + file,
     debug: true
+  }).transform("babelify", {
+    presets: ["env"]
   });
 
-  return b.bundle()
+  let currentBundle = b
+    .bundle()
     .pipe(source(file))
-    .pipe(buffer())
-    // .pipe(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
-        // .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/script/'));
+    .pipe(buffer());
+
+  if (isProd) {
+    currentBundle = currentBundle.pipe(uglify());
+  }
+
+  return currentBundle
+    .on("error", gutil.log)
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./dist/script/"));
 }
 
-gulp.task('bundle', function () {
-  // set up the browserify instance on a task basis
-  bundle('cms.js');
-  bundle('embeded.js');
+gulp.task("js", function() {
+  let contact = gulp.src("src/script/contact.js");
+
+  if (isProd) {
+    contact = contact.pipe(uglify());
+  }
+
+  return merge(
+    bundle("cms.js"),
+    bundle("embeded.js"),
+    contact.pipe(gulp.dest("./dist/script"))
+  );
 });
 
-gulp.task('copy-files', function() {
-    return gulp.src(['src/**/*', '!src/**/*.html', '!src/script/**/*.*'])
-        .pipe(gulp.dest('./dist'));
+gulp.task("copy-files", function() {
+  return gulp
+    .src(["src/**/*", "!src/**/*.html", "!src/script/**/*.*", "!src/**/*.css"])
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('copy-html', function() {
-    return gulp.src('src/**/*.html')
-        .pipe(handlebars(getData(), {}))
-        .pipe(gulp.dest('./dist'));
+gulp.task("html", function() {
+  return gulp
+    .src("src/**/*.html")
+    .pipe(handlebars(getData(), {}))
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build', ['copy-files', 'copy-html', 'bundle']);
+gulp.task("css", function() {
+  return gulp
+    .src("src/**/*.css")
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./dist"));
+});
 
-gulp.task('default', ['build'], function(){
-  http.createServer(
-    ecstatic({ root: __dirname + '/dist' })
-  ).listen(8080);
+gulp.task("reload", function() {
+  reload();
+});
 
-  gulp.watch('src/**/*.*', function(){
-    gulp.run('build');
+gulp.task("reload-js", ["js"], reload);
+gulp.task("reload-css", ["css"], reload);
+gulp.task("reload-html", ["html"], reload);
+gulp.task("reload-copy-files", ["copy-files"], reload);
+
+gulp.task("default", ["copy-files", "html", "js", "css"], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
   });
+
+  gulp.watch(
+    ["src/**/*", "!src/**/*.html", "!src/script/**/*.*", "!src/**/*.css"],
+    ["reload-copy-files"]
+  );
+  gulp.watch("src/**/*.css", ["reload-css"]);
+  gulp.watch("src/script/**/*.*", ["reload-js"]);
+  gulp.watch("src/**/*.html", ["reload-html"]);
 });
